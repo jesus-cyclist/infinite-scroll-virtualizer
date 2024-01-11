d2faffedf43a05a3c75d96e36f1f3ee4d121650a
Ильин Николай <gttravel@mail.ru>
2024-01-11 23:24:39 +0300

    init virtualizer

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import { MainPage, PostPage } from 'pages'
 import { ForbiddenPage } from 'pages/forbidden-page/ui/Page'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import styles from './styles/App.module.scss'
+import { RouteName } from 'shared'
 
 export function App() {
   const location = useLocation()
@@ -10,13 +11,13 @@ export function App() {
   return (
     <div className={styles.app}>
       <Routes location={state?.post || location}>
-        <Route path={'/'} element={<MainPage />} />
-        <Route path={`/post/:id`} element={<PostPage />} />
-        <Route path={'*'} element={<ForbiddenPage />} />
+        <Route path={RouteName.MAIN_PAGE} element={<MainPage />} />
+        <Route path={RouteName.POST_PAGE} element={<PostPage />} />
+        <Route path={RouteName.FORBIDDEN_PAGE} element={<ForbiddenPage />} />
       </Routes>
       {state?.post && (
         <Routes>
-          <Route path={`/post/:id`} element={<PostPage />} />
+          <Route path={RouteName.POST_PAGE} element={<PostPage />} />
         </Routes>
       )}
     </div>
---
+++ src/shared/config/routes/routes.ts
@@ -0,0 +1,5 @@
+export enum RouteName {
+  MAIN_PAGE = 'infinite-scroll-virtualizer/',
+  POST_PAGE = 'infinite-scroll-virtualizer/post/:id',
+  FORBIDDEN_PAGE = '*',
+}
